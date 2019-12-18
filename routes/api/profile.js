2004326e9457f4c914509a51be03d78718a818ac
beovulf <pcichon88@gmail.com>
2019-12-18 17:54:35 +0100

    Added user endpoints with authentication

---
+++ routes/api/profile.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+
+// @route GET api/profile/me
+// @desc Get current users profile
+// @access Private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.ud
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route GET api/profile
+// @desc Test route
+// @access Public
+
+router.get("/", (req, res) => res.send("Profile route"));
+
+module.exports = router;
