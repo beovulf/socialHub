085eb26c9cc0ccaa5d321fd6710983380ea7a5a4
beovulf <pcichon88@gmail.com>
2020-01-07 11:21:16 +0100

    Alert component setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import { Register } from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+import Register from "./components/auth/Register";
 import { Login } from "./components/auth/Login";
 
 // Redux
@@ -19,6 +20,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-export const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -19,7 +22,8 @@ export const Register = () => {
     e.preventDefault();
 
     if (password !== password2) {
-      console.log("Passwords do not match");
+      // console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
       const newUSer = {
         name,
@@ -104,3 +108,9 @@ export const Register = () => {
     </Fragment>
   );
 };
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
