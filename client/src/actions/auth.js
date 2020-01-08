36fb491c77b4a95e3733e75a6b7da3030bf3d8b8
beovulf <pcichon88@gmail.com>
2020-01-08 08:36:47 +0100

    Added logout functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -94,4 +95,10 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout user, clear profile
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
