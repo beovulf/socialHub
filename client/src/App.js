a5696aa415084a1ba9606bb0dc2dcc16b99ed1e7
beovulf <pcichon88@gmail.com>
2020-01-09 17:18:59 +0100

    Add and edit profile functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -38,6 +40,16 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
             </Switch>
           </section>
         </Fragment>
