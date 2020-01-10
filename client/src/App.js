1a06c809aa3fa7e18eb0a7f367e4b337e990ea26
beovulf <pcichon88@gmail.com>
2020-01-10 08:24:00 +0100

    List profile education and experience

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -49,6 +51,16 @@ const App = () => {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </section>
