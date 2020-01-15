fff32532346cb277da53fe5ceb862234083f6cd2
beovulf <pcichon88@gmail.com>
2020-01-15 09:23:56 +0100

    Beginning of formik integration

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,37 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addComment } from "../../actions/post";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const CommentForm = ({ postId, addComment }) => {
-  const [text, setText] = useState("");
-  return (
-    <div class="post-form">
-      <div class="bg-primary p">
-        <h3>Leave a Comment</h3>
-      </div>
-      <form
-        class="form my-1"
-        onSubmit={e => {
-          e.preventDefault();
-          addComment(postId, { text });
-          setText("");
-        }}
-      >
-        <textarea
-          name="text"
-          cols="30"
-          rows="5"
-          placeholder="Create a comment"
-          value={text}
-          onChange={e => setText(e.target.value)}
-          required
-        ></textarea>
-        <input type="submit" class="btn btn-dark my-1" value="Submit" />
-      </form>
+const CommentForm = ({ postId, addComment }) => (
+  <div class="post-form">
+    <div class="bg-primary p">
+      <h3>Leave a Comment</h3>
     </div>
-  );
-};
+    <Formik
+      initialValues={{ text: "" }}
+      validate={values => {
+        const errors = {};
+        if (!values.text) {
+          errors.text = "Required";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { resetForm }) => {
+        addComment(postId, { text: values.text });
+        resetForm();
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="form my-1">
+          <Field name="text" component="textarea" />
+          <ErrorMessage name="text" component="div" />
+          <input
+            type="submit"
+            disabled={isSubmitting}
+            class="btn btn-dark my-1"
+            value="Submit"
+          />
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
 CommentForm.propTypes = {
   addComment: PropTypes.func.isRequired
