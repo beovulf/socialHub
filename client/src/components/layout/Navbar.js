62f838c663087c0463dea5ea7cd3b5a43bfe38ef
beovulf <pcichon88@gmail.com>
2020-01-16 22:25:28 +0100

    Added changes to styles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          <i className="fas fa-code"></i> SocialHub
         </Link>
       </h1>
       {!loading && (
